6b4d6fee6458c8641a8b2db4d3797573cbc1c972
namunanamu <a11111111@aoyama.jp>
2023-10-02 21:43:15 +0900

    プレイヤーが移動できない床の実装
    指定したマップチップにはプレイヤーが移動できないようにした。
    
    branch#05

--- js/game/common_class/player.js
+++ js/game/common_class/player.js
@@ -1,16 +1,26 @@
 /* プレイヤーキャラクターを表すクラス */
 
 import { Arrow } from "./arrow.js";
+import { world_map } from "./../common_function/world_map.js";
 
 const PLAYER_SPEED_COEFFICIENT = 0.33;    // 弓矢のスピードの係数
 const ATTACK_COOL_TIME = 10; // 攻撃クールタイム
+const MAP_CHIP_WHICH_PLAYER_CANNOT_MOVE_ON = [ // プレイヤーが移動できない床
+    2,  // 海
+    4,  // 剣のマークの看板
+    8,  // 木
+    9,  // 岩
+    10, // テーブル
+    11, // 石レンガ
+    15, // 石レンガ(壁)
+];
 
 export class Player{
     
     // コンストラクタ
     constructor(x, y, world_map_x, world_map_y, img){
-        this.x = x;                         // x 座標(タイル基準 = 一番左が 1, 一番右が 16), プレイヤーの画像の中心の座標とする
-        this.y = y;                         // y 座標(タイル基準 = 一番上が 1, 一番下が 16), プレイヤーの画像の中心の座標とする
+        this.x = x;                         // x 座標(タイル基準 = 一番左が 0, 一番右が 16), プレイヤーの画像の中心の座標とする
+        this.y = y;                         // y 座標(タイル基準 = 一番上が 0, 一番下が 16), プレイヤーの画像の中心の座標とする
         this.world_map_x = world_map_x;     // 現在プレイヤーがいる ワールドマップの x 座標
         this.world_map_y = world_map_y;     // 現在プレイヤーがいる ワールドマップの y 座標
         this.img = img;                     // 写真 (front: 正面, back: 背面, left: 左, right: 右)
@@ -29,20 +39,24 @@ export class Player{
         if(this.in_action_frame.move <= 0){
             // w, a, s, d キー入力に応じて移動させる
             if(key.is_w_pressed){ 
-                this.direction = 0;
+                this.direction = 0; // 上方向
                 this.in_action_frame.move = 3; // 3フレーム使って実際には移動する
+                this.check_movability(); // 上方向に移動可能かどうか確かめる
             }
             else if(key.is_s_pressed){
-                this.direction = 1;
+                this.direction = 1; // 下方向
                 this.in_action_frame.move = 3; // 3フレーム使って実際には移動する
+                this.check_movability(); // 下方向に移動可能かどうか確かめる
             }
             else if(key.is_a_pressed){
-                this.direction = 2;
+                this.direction = 2; // 左方向
                 this.in_action_frame.move = 3; // 3フレーム使って実際には移動する
+                this.check_movability(); // 左方向に移動可能かどうか確かめる
             }
             else if(key.is_d_pressed){
-                this.direction = 3;
+                this.direction = 3; // 右方向
                 this.in_action_frame.move = 3; // 3フレーム使って実際には移動する
+                this.check_movability(); // 右方向に移動可能かどうか確かめる
             }
         }
 
@@ -50,6 +64,102 @@ export class Player{
         if(this.in_action_frame.attack <= 0){
             if(key.is_enter_pressed){
                 this.in_action_frame.attack = ATTACK_COOL_TIME; // 攻撃クールタイムがある
+            }
+        }
+    }
+
+    // 進もうとしている方向に進めるかどうか確かめる
+    // 進めない例: 移動しようとしている方向に、移動できない床がある場合など。
+    check_movability(){
+        // 現在プレイヤーが居るマップ
+        let current_map = world_map()[this.world_map_x][this.world_map_y];
+        let player_x = this.x - 0.5; // プレイヤーの x 座標を配列のインデックスになるように調整。一番左上のタイルの真上に経っていた場合、0
+        let player_y = this.y - 0.5; // プレイヤーの y 座標を配列のインデックスになるように調整。一番左上のタイルの真上に経っていた場合、0
+
+        // 上方向に移動しようとしている場合
+        if(this.direction == 0){
+            // プレイヤーが上下において、ぴったりタイルの上に乗っている場合、絶対上に一歩進めるので、チェックはスルー
+            if(player_y % 1 == 0) return;
+
+            // もしマップの上端の場合、絶対上に一歩進めるので、チェックはスルー
+            if(player_y <= 0) return;
+            
+            let upper_left = { // プレイヤーの上の左側にある床（プレイヤーが左右において、ぴったりタイルの上に乗っている場合、真上の床を指す）
+                x: Math.floor(player_x),
+                y: player_y - 0.5,
+            };
+            let upper_right = { // プレイヤーの上の右側にある床（プレイヤーが左右において、ぴったりタイルの上に乗っている場合、真上の床を指す）
+                x: Math.ceil(player_x),
+                y: player_y - 0.5,
+            };
+
+            // プレイヤーの半歩上にある床が移動できない場合
+            if(MAP_CHIP_WHICH_PLAYER_CANNOT_MOVE_ON.includes(current_map[upper_left.y][upper_left.x]) || 
+            MAP_CHIP_WHICH_PLAYER_CANNOT_MOVE_ON.includes(current_map[upper_right.y][upper_right.x])){
+                // 移動は行わない
+                this.in_action_frame.move = 0;
+            }
+        }
+        // 下方向に移動しようとしている場合
+        if(this.direction == 1){
+            // プレイヤーが上下において、ぴったりタイルの上に乗っていない場合、絶対下に一歩進めるので、チェックはスルー
+            if(Math.abs((player_y) % 1) == 0.5) return; // NOTE: -0.5 % 1 は 0.5 ではなく、-0.5 となってしまうので、絶対値を取って回避。
+
+            // もしマップの下端の場合、絶対下に一歩進めるので、チェックはスルー
+            if(player_y >= FIELD_SIZE_IN_SCREEN - 1) return;
+
+            let lower_left = { // プレイヤーの下の左側にある床（プレイヤーが左右において、ぴったりタイルの上に乗っている場合、真下の床を指す）
+                x: Math.floor(player_x),
+                y: player_y + 1,
+            };
+            let lower_right = { // プレイヤーの下の右側にある床（プレイヤーが左右において、ぴったりタイルの上に乗っている場合、真下の床を指す）
+                x: Math.ceil(player_x),
+                y: player_y + 1,
+            };
+            
+            // プレイヤーの 1歩下にある床が移動できない場合
+            if(MAP_CHIP_WHICH_PLAYER_CANNOT_MOVE_ON.includes(current_map[lower_left.y][lower_left.x]) || 
+            MAP_CHIP_WHICH_PLAYER_CANNOT_MOVE_ON.includes(current_map[lower_right.y][lower_right.x])){
+                // 移動は行わない
+                this.in_action_frame.move = 0;
+            }
+        }
+        // 左方向に移動しようとしている場合
+        if(this.direction == 2){
+            // プレイヤーが左右において、ぴったりタイルの上に乗っていない場合、絶対左に一歩進めるので、チェックはスルー
+            if((player_x) % 1 == 0.5) return;
+
+            let left = { // プレイヤーの左の下側にある床（プレイヤーが上下において、ぴったりタイルの上に乗っている場合、真左の床を指す）
+                x: player_x - 1,
+                y: Math.ceil(player_y),
+            };
+
+            // もしマップの下端の場合、参照する床はプレイヤーの左の上側にする
+            if(player_y >= FIELD_SIZE_IN_SCREEN - 1) left.y = Math.floor(player_y);
+
+            // プレイヤーの 1歩左にある床が移動できない場合
+            if(MAP_CHIP_WHICH_PLAYER_CANNOT_MOVE_ON.includes(current_map[left.y][left.x])){
+                // 移動は行わない
+                this.in_action_frame.move = 0;
+            }
+        }
+        // 右方向に移動しようとしている場合
+        if(this.direction == 3){
+            // プレイヤーが左右において、ぴったりタイルの上に乗っていない場合、絶対右に一歩進めるので、チェックはスルー
+            if((player_x) % 1 == 0.5) return;
+
+            let right = { // プレイヤーの右の下側にある床（プレイヤーが上下において、ぴったりタイルの上に乗っている場合、真右の床を指す）
+                x: player_x + 1,
+                y: Math.ceil(player_y),
+            };
+
+            // もしマップの下端の場合、参照する床はプレイヤーの右の上側にする
+            if(player_y >= FIELD_SIZE_IN_SCREEN - 1) right.y = Math.floor(player_y);
+
+            // プレイヤーの 1歩右にある床が移動できない場合
+            if(MAP_CHIP_WHICH_PLAYER_CANNOT_MOVE_ON.includes(current_map[right.y][right.x])){
+                // 移動は行わない
+                this.in_action_frame.move = 0;
             }
         }
     }
