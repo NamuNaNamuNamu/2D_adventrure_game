1ae996a3d49b94b09afc989e98c85f3520a37e03
namunanamu <a11111111@aoyama.jp>
2024-03-23 14:19:04 +0900

    砂原ゾーンの実装
    [4][7] ~ [7][9]までのエリアの実装
    
    branch#10

--- js/game/common_function/world_map.js
+++ js/game/common_function/world_map.js
@@ -42,9 +42,17 @@ import { map_x3_y7 } from "./world_map_parts/map_x3_y7.js";
 import { map_x3_y8 } from "./world_map_parts/map_x3_y8.js";
 import { map_x3_y9 } from "./world_map_parts/map_x3_y9.js";
 
+import { map_x4_y7 } from "./world_map_parts/map_x4_y7.js";
+import { map_x4_y8 } from "./world_map_parts/map_x4_y8.js";
+import { map_x4_y9 } from "./world_map_parts/map_x4_y9.js";
+
 import { map_x5_y0 } from "./world_map_parts/map_x5_y0.js";
 import { map_x5_y1 } from "./world_map_parts/map_x5_y1.js";
 import { map_x5_y2 } from "./world_map_parts/map_x5_y2.js";
+
+import { map_x5_y7 } from "./world_map_parts/map_x5_y7.js";
+import { map_x5_y8 } from "./world_map_parts/map_x5_y8.js";
+import { map_x5_y9 } from "./world_map_parts/map_x5_y9.js";
 
 import { map_x5_y10 } from "./world_map_parts/map_x5_y10.js";
 
@@ -52,9 +60,17 @@ import { map_x6_y0 } from "./world_map_parts/map_x6_y0.js";
 import { map_x6_y1 } from "./world_map_parts/map_x6_y1.js";
 import { map_x6_y2 } from "./world_map_parts/map_x6_y2.js";
 
+import { map_x6_y7 } from "./world_map_parts/map_x6_y7.js";
+import { map_x6_y8 } from "./world_map_parts/map_x6_y8.js";
+import { map_x6_y9 } from "./world_map_parts/map_x6_y9.js";
+
 import { map_x7_y0 } from "./world_map_parts/map_x7_y0.js";
 import { map_x7_y1 } from "./world_map_parts/map_x7_y1.js";
 import { map_x7_y2 } from "./world_map_parts/map_x7_y2.js";
+
+import { map_x7_y7 } from "./world_map_parts/map_x7_y7.js";
+import { map_x7_y8 } from "./world_map_parts/map_x7_y8.js";
+import { map_x7_y9 } from "./world_map_parts/map_x7_y9.js";
 
 import { map_x8_y0 } from "./world_map_parts/map_x8_y0.js";
 import { map_x8_y1 } from "./world_map_parts/map_x8_y1.js";
@@ -99,10 +115,10 @@ export function world_map(){
         [map_x1_y0(), map_x1_y1(), map_x1_y2(), map_x1_y3(), map_x1_y4(), map_x1_y5(), map_x1_y6(), map_x1_y7(), map_x1_y8(), map_x1_y9(), 0, 0, 0, 0, 0],
         [map_x2_y0(), map_x2_y1(), map_x2_y2(), map_x2_y3(), map_x2_y4(), map_x2_y5(), map_x2_y6(), map_x2_y7(), map_x2_y8(), map_x2_y9(), 0, 0, 0, 0, map_x2_y14()],
         [0, 0, 0, 0, 0, 0, 0, map_x3_y7(), map_x3_y8(), map_x3_y9(), 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [map_x5_y0(), map_x5_y1(), map_x5_y2(), 0, 0, 0, 0, 0, 0, 0, map_x5_y10(), 0, 0, 0, 0],
-        [map_x6_y0(), map_x6_y1(), map_x6_y2(), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [map_x7_y0(), map_x7_y1(), map_x7_y2(), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, map_x4_y7(), map_x4_y8(), map_x4_y9(), 0, 0, 0, 0, 0],
+        [map_x5_y0(), map_x5_y1(), map_x5_y2(), 0, 0, 0, 0, map_x5_y7(), map_x5_y8(), map_x5_y9(), map_x5_y10(), 0, 0, 0, 0],
+        [map_x6_y0(), map_x6_y1(), map_x6_y2(), 0, 0, 0, 0, map_x6_y7(), map_x6_y8(), map_x6_y9(), 0, 0, 0, 0, 0],
+        [map_x7_y0(), map_x7_y1(), map_x7_y2(), 0, 0, 0, 0, map_x7_y7(), map_x7_y8(), map_x7_y9(), 0, 0, 0, 0, 0],
         [map_x8_y0(), map_x8_y1(), map_x8_y2(), map_x8_y3(), 0, map_x8_y5(), map_x8_y6(), map_x8_y7(), map_x8_y8(), map_x8_y9(), 0, 0, 0, 0, 0],
         [map_x9_y0(), map_x9_y1(), map_x9_y2(), map_x9_y3(), 0, map_x9_y5(), map_x9_y6(), map_x9_y7(), map_x9_y8(), map_x9_y9(), 0, 0, 0, 0, 0],
     ];
